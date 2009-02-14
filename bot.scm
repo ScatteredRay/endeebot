e79148c5605b65361e37d6278123459cc1716346
Nicholas Indy Ray <indy@ishere.com>
2009-02-14 02:25:20 -0800

    Bot Additions.
    Mostly op functionality, also bot utility functions.

--- bot.scm
+++ bot.scm
@@ -11,68 +11,141 @@
 (tcp-read-timeout #f)
 
 (require-extension irc)
+(require-extension sandbox)
 
 (define bot-name "endeebot")
 (define bot-server "irc.efnet.ch")
 ;(set! bot-server "efnet.xs4all.nl")
+(define channel-list '("#weightedsixes"))
+(define channel-name "#weightedsixes") ; only support one channel atm
+
+(define op-file "channel.ops")
+(define op-list '())
+
+(define (read-op-list) (set! op-list (with-input-from-file op-file
+                                       read)))
+
+(define (write-op-list) (with-output-to-file op-file
+                                               (lambda () (write op-list))))
+
+(read-op-list)
+
+(define (join-channels lst)
+  (if (not (null? lst))
+      (begin
+        (irc:join con (car lst))
+        (join-channels (cdr lst)))
+      '()))
+
+(define (message-body message)
+  (let ((parameters (cadr (irc:message-parameters message))))
+    (if (irc:extended-data? parameters)
+        (irc:extended-data-content parameters)
+        parameters)))
+
+(define (message-hostmask msg)
+  (string-append (cadr (irc:message-prefix msg)) "@" (caddr (irc:message-prefix msg))))
+
+(define (is-op? name host)
+  (find
+   (lambda (op)
+     (and (equal? (car op) name)
+          (string-match (cdr op) host)))
+   op-list))
+
+(define (message-dest msg)
+  (if (equal? bot-name (irc:message-receiver msg))
+      (irc:message-sender msg)
+      channel-name))
 
 (define con (irc:connection nick: bot-name server: bot-server))
-
 (irc:connect con)
 
-;; (define (dump-message msg)
-;;   (display (irc:message-prefix msg))
-;;   (newline)
-;;   (display (irc:message-command msg))
-;;   (newline)
-;;   (display (irc:message-code msg))
-;;   (newline)
-;;   (display (irc:message-body msg))
-;;   (newline))
-
-;; (define curr-msg '())
-
-;; (define (wait-dump)
-;;   (set! curr-msg (irc:wait con))
-;;   (dump-message curr-msg))
-
-;(define (bot-eval msg))
-
-(irc:join con "#weightedsixes")
-
-(irc:say con "Hello" "Arelius")
+(join-channels channel-list)
 
 ;(irc:remove-message-handler! con 'QUIT)
 
 (irc:add-message-handler! con (lambda (msg) (display "quit!") (irc:quit con "Bye") (exit))
-                          sender: (lambda (sender) (or (equal? sender "Arelius") (equal? sender "pf_")))
+                          sender: (lambda (sender) (equal? sender "Arelius"))
                           body: (string-append "PRIVMSG .* :" bot-name "[,: ]*QUIT") tag: 'QUIT)
+
+
+;(irc:remove-message-handler! con 'op)
+
+(irc:add-message-handler! con (lambda (msg)
+                                (if (is-op? (irc:message-sender msg) (message-hostmask msg))
+                                    (irc:command
+                                     con
+                                     (string-append "mode " channel-name " +o " (irc:message-sender msg)))))
+                          command: "JOIN"
+                          tag: 'op)
+
+(define (get-bot-command msg)
+  (let ((out (string-match (string-append
+                            "^" bot-name "[,:]?[ ]*(.*)")
+                           (message-body msg))))
+    (if out
+        (cadr out)
+        #f)))
+
+(define (run-admin-command msg)
+  (let ((cmd (string-match
+              "add op[ ]*([^ ]*)[ ]*([^ ]*)"
+              (get-bot-command msg))))
+    (if cmd
+        (begin
+          (set! op-list
+                (cons
+                 (cons (cadr cmd) (caddr cmd))
+                 op-list))
+          (write-op-list)
+          (irc:say con
+                   (string-append
+                    "Added user \"" (cadr cmd) "\" to operator list.")
+                   (message-dest msg)))
+        '())))
+
+;(irc:remove-message-handler! con 'admin)
+
+(irc:add-message-handler! con (lambda (msg)
+                                (if (is-op? (irc:message-sender msg) (message-hostmask msg))
+                                    (if (get-bot-command msg)
+                                        (if (run-admin-command msg)
+                                            #t
+                                            #f)
+                                        #f))
+                                #f)
+                          body: bot-name
+                          command: "PRIVMSG"
+                          tag: 'admin)
 
 ;(irc:remove-message-handler! con 'eval)
 
 (irc:add-message-handler! con (lambda (msg)
-                                (handle-exceptions exn
-                                                   '()
-                                                   (let ((search (string-search
-                                                                  (string-append "PRIVMSG (.*) :" bot-name "[,: ]*eval(.*)")
-                                                                  (irc:message-body msg))))
+                                (let* ((search (string-search
+                                               (string-append "PRIVMSG (.*) :" bot-name "[,: ]*eval(.*)")
+                                               (irc:message-body msg)))
+                                      (resp-dest (message-dest msg)))
+                                  (handle-exceptions exn
+                                                     (irc:say con
+                                                              "Error in eval!"
+                                                              resp-dest)
                                                      (irc:say con
                                                               (->string
-                                                               (eval
+                                                               (safe-eval
                                                                 (read
                                                                  (open-input-string
-                                                                  (caddr search)))))
-                                                              (if (equal? bot-name (cadr search))
-                                                                  (irc:message-sender msg)
-                                                                  (cadr search))))))
-                          sender: (lambda (sender) (equal? sender "Arelius"))
-                          body: (string-append "PRIVMSG (.*) :" bot-name "[,: ]*eval(.*)") tag: 'eval)
+                                                                  (caddr search)))
+                                                                fuel: 1000
+                                                                allocation-limit: 1048576))
+                                                              resp-dest))))
+                          sender: (lambda (sender) #t)
+                          body: (string-append "PRIVMSG (.*) :" bot-name "[,: ]*eval(.*)")
+                          tag: 'eval
+                          command: "PRIVMSG")
 
 
 (define (run) (irc:run-message-loop con debug: #t pong: #t))
 
 (run)
-;(while (#t) (handle-exceptions exn '() (run))) ; To Catch rrepl cause exceptions
-
-;(irc:quit con "Buh bye")
-;(irc:disconnect con)
+;(while (#t) (handle-exceptions exn '() (run))) ; To Catch rrepl caused exceptions
